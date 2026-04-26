804b23c62ba62ddbb10c696fd75caca8b5bf8544
Yamuna <hyamuna2807@gmail.com>
2026-04-26 18:32:00 +0530

    Add files via upload

---
+++ forloop17.py
@@ -0,0 +1,8 @@
+students=["Tarun","Varun","Arun"]
+marks=[56,78,90]
+
+students_marks= {}
+
+for i in range(len(students)):
+    students_marks[students[i]]=marks[i]
+print(students_marks)    
---
+++ forloop18.py
@@ -0,0 +1,7 @@
+names=["Rama","Krishna","Shiva"]
+partners=["Seete","Radhe","Parvathi"]
+
+couples={}
+for i in range(len(names)):
+    couples[names[i]]=partners[i]
+print(couples)    
