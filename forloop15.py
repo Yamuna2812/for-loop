a8c42624dddbe9e0c9244c0c531bdbe8a1d8c445
Yamuna <hyamuna2807@gmail.com>
2026-04-26 18:07:57 +0530

    Add files via upload

---
+++ forloop15.py
@@ -0,0 +1,3 @@
+branch_atgopalan={"CSE AIML":120,"ECE":60,"CSE":120,"ISE":60,"AE":60,"DS":60}
+for branch in branch_atgopalan.keys():
+    print(branch)
---
+++ forloop16.py
@@ -0,0 +1,3 @@
+numbers={"one":1,"two":2,"three":3,"four":4}
+for num in numbers.values():
+    print(num)
